58d450bffe5ac942d9c062328370b25da9455f17
Sergey-Labzhinskyi <labshinskiy@gmail.com>
2020-04-20 22:26:22 +0300

    test stash

--- app/src/main/java/com/bignerdranch/rxjava/BooksActivity.java
+++ app/src/main/java/com/bignerdranch/rxjava/BooksActivity.java
@@ -1,6 +1,26 @@
 package com.bignerdranch.rxjava;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BooksActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
